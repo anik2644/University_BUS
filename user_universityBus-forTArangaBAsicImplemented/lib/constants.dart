4a96416e5cc90939135d8018972333c308673f0d
anik <anik11556@gmail.com>
2023-05-12 16:45:58 +0600

    done

--- user_universityBus-forTArangaBAsicImplemented/lib/constants.dart
+++ user_universityBus-forTArangaBAsicImplemented/lib/constants.dart
@@ -14,7 +14,7 @@ const kContentColorDarkTheme = Color(0xFFF5FCF9);
 const kWarninngColor = Color(0xFFF3BB1C);
 const kErrorColor = Color(0xFFF03738);
 const kDefaultPadding = 20.0;
-
+//anik
 
 class AllStaticVariables{
 
